2dff93b372d1b9b42a6e87bea707f58c71200981
cssher <sherali2910.sa@gmail.com>
2021-07-12 13:46:14 +0530

    added alert for empty selections

--- src/components/flight-form/FlightForm.jsx
+++ src/components/flight-form/FlightForm.jsx
@@ -3,6 +3,24 @@ import styles from "./FlightForm.module.css";
 import axios from "axios";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import { RiExchangeFill } from "react-icons/ri";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
 
 function debounce(func, wait) {
   let timeout;
@@ -72,83 +90,137 @@ export const FlightForm = () => {
     e.preventDefault();
     const originValue = document.querySelector("#origin_id").value;
     const destinationValue = document.querySelector("#destination_id").value;
-    const selectedOriginVal = originDropdownData?.Places.filter(
-      (place) => place.PlaceName === originValue
-    );
-    const selectedDestinationVal = destinationDropdownData?.Places.filter(
-      (place) => place.PlaceName === destinationValue
-    );
+    if (originValue && destinationValue) {
+      const selectedOriginVal = originDropdownData?.Places.filter(
+        (place) => place.PlaceName === originValue
+      );
+      const selectedDestinationVal = destinationDropdownData?.Places.filter(
+        (place) => place.PlaceName === destinationValue
+      );
 
-    let originCode = selectedOriginVal[0].PlaceId;
-    let destinationCode = selectedDestinationVal[0].PlaceId;
+      let originCode = selectedOriginVal[0].PlaceId;
+      let destinationCode = selectedDestinationVal[0].PlaceId;
 
-    console.log(originCode, destinationCode);
+      console.log(originCode, destinationCode);
 
-    const options = {
-      method: "GET",
-      url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/IN/INR/en-US/${originCode}/${destinationCode}/2021-07-15`,
-      headers: {
-        "x-rapidapi-key": "f121a9893dmshde3fc94e02a4a9dp11446djsn6564a3d4ff58",
-        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-      },
-    };
+      const options = {
+        method: "GET",
+        url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/IN/INR/en-US/${originCode}/${destinationCode}/2021-07-15`,
+        headers: {
+          "x-rapidapi-key":
+            "f121a9893dmshde3fc94e02a4a9dp11446djsn6564a3d4ff58",
+          "x-rapidapi-host":
+            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        },
+      };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+    else if(originValue.length < 1 || destinationValue.length < 1) {
+      setOpen(true);
+    }
+  };
+
+  const swapPlaces = () => {
+    const originValue = document.querySelector("#origin_id").value;
+    const destinationValue = document.querySelector("#destination_id").value;
+    if (originValue && destinationValue) {
+      let destination = originValue;
+      let origin = destinationValue;
+      document.querySelector("#origin_id").value = origin;
+      document.querySelector("#destination_id").value = destination;
+    }
+  };
+
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
   };
 
   return (
     <div className={styles.form_container}>
-      <form onSubmit={handleSubmit}>
-        <Autocomplete
-          id="origin_id"
-          freeSolo
-          size="small"
-          options={
-            originDropdownData?.Places
-              ? originDropdownData.Places
-              : defaultData1
-          }
-          getOptionLabel={(option) => option.PlaceName}
-          style={{ width: 200 }}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              onChange={(e) => debouncedOrigin(e.target.value)}
-              label="Origin"
-              variant="outlined"
+      <div className={styles.form_div}>
+        
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="warning">
+          Please select both origin & destination before proceeding.
+        </Alert>
+      </Snackbar>
+        
+        <form onSubmit={handleSubmit} className={styles.flight_form}>
+          <div className={styles.origin_autocomplete}>
+            <Autocomplete
+              id="origin_id"
+              freeSolo
+              options={
+                originDropdownData?.Places
+                  ? originDropdownData.Places
+                  : defaultData1
+              }
+              getOptionLabel={(option) => option.PlaceName}
+              style={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  onChange={(e) => debouncedOrigin(e.target.value)}
+                  label="Origin"
+                  variant="outlined"
+                />
+              )}
             />
-          )}
-        />
+          </div>
 
-        <Autocomplete
-          id="destination_id"
-          freeSolo
-          size="small"
-          options={
-            destinationDropdownData?.Places
-              ? destinationDropdownData.Places
-              : defaultData2
-          }
-          getOptionLabel={(option) => option.PlaceName}
-          style={{ width: 200 }}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              onChange={(e) => debouncedDestination(e.target.value)}
-              label="Destination"
-              variant="outlined"
+          <div className={styles.swap_places}>
+            <RiExchangeFill
+              onClick={swapPlaces}
+              style={{ fontSize: "50px", cursor: "pointer" }}
             />
-          )}
-        />
-        <button>Search</button>
-      </form>
+          </div>
+
+          <div className={styles.origin_autocomplete}>
+            <Autocomplete
+              id="destination_id"
+              freeSolo
+              options={
+                destinationDropdownData?.Places
+                  ? destinationDropdownData.Places
+                  : defaultData2
+              }
+              getOptionLabel={(option) => option.PlaceName}
+              style={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  onChange={(e) => debouncedDestination(e.target.value)}
+                  label="Destination"
+                  variant="outlined"
+                />
+              )}
+            />
+          </div>
+
+          <div className={styles.search_flight_btn_div}>
+            <button className={styles.search_flight_btn}>Search</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
